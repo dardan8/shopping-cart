8cbee0899d6a71770dd3f94cdb2b9c535e9c8cce
dardan8 <dardan.dara@gmail.com>
2023-01-03 16:05:20 +0100

    Home page added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+
+import "./home.css";
+import HeroImage from "../../public/images/hero-image.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className='home-container'>
+      <div className='left'>
+        <h1>Shop your favorite tech tools</h1>
+        <h3>
+          A wide selection of all the technology tools you need to enhance your
+          daily work. 100% authentic products at the best price.
+        </h3>
+        <Link to='/store'>
+          <button className='shop-button-primary'>Go to Store</button>
+        </Link>
+      </div>
+      <div className='right'>
+        <Link to='/store'>
+          <img
+            src={HeroImage}
+            alt='iPhone mockup from uimaterial.com/mockups'
+            className='hero-image'
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
