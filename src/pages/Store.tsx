521ba41b8d01be260c4a6571567e4aea7279a8a7
dardan8 <dardan.dara@gmail.com>
2023-01-07 18:14:33 +0100

    route updates

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,19 +1,18 @@
-import "./store.css"
+import "./store.css";
 
-import storeItems from "../data/items.json"
-import { StoreItem } from '../components/StoreItem';
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
 
 const Store = () => {
   console.log(storeItems);
 
   return (
     <div className='store-container'>
-        {storeItems.map(item => (
-              <StoreItem {...item}/>
-          )
-        )}
+      {storeItems.map((item) => (
+        <StoreItem {...item} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Store
+export default Store;
